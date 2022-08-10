1863d8387426f839a2da5aba972fc21b2a26e577
sukhdev-bajiya <bajiya4442@gmail.com>
2022-08-10 13:20:00 +0530

    view page 70% done

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,1 +1,12 @@
 document.getElementById("newsNavbarIten").style.fill = "#e31b46";
+
+let inputValFromHomePage;
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  inputValFromHomePage = document.getElementById("searchInputOfView").value;
+
+  // console.log(inputValFromHomePage);
+  localStorage.setItem("searchValue", inputValFromHomePage);
+  document.getElementById("searchInputOfView").value =
+    localStorage.getItem("searchValue");
+});
--- assets/js/view.js
+++ assets/js/view.js
@@ -1,1 +1,54 @@
 document.getElementById("allNavbarIten").style.fill = "#e31b46";
+
+let inputValFromHomePage;
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  inputValFromHomePage = document.getElementById("searchInputOfView").value;
+
+  // console.log(inputValFromHomePage);
+  localStorage.setItem("searchValue", inputValFromHomePage);
+  document.getElementById("searchInputOfView").value =
+    localStorage.getItem("searchValue");
+});
+
+displayData();
+
+async function displayData() {
+  try {
+    let res = await fetch("https://jsonservermasai.herokuapp.com/brands");
+    let data = await res.json();
+
+    document.getElementById("showSearchDataSection").innerHTML = `
+    <div id="showrequestDataCount">About <span>${data[0].searchInformation.formattedTotalResults}</span> results (<span>${data[0].searchInformation.formattedSearchTime}</span> seconds)</div>
+    <div id="searchItemsList"></div>
+    `;
+    data[0].items.forEach((element) => {
+      let div = document.createElement("div");
+      let urlPart = element.link.length - 1;
+
+      let picUrl = "./assets/img/05.png";
+
+      if (element.pagemap.cse_thumbnail != undefined) {
+        picUrl = element.pagemap.cse_thumbnail[0].src;
+      }
+      div.innerHTML = `
+      <div>
+          <a href="${element.link.slice(
+            0,
+            urlPart
+          )}" target="_blank" id="searchUrl">${element.displayLink}</a>
+          <a href="${element.link.slice(
+            0,
+            urlPart
+          )}" target="_blank" id="searchTitel">${element.htmlTitle}</a>
+          <p id="searchDescription">${element.htmlSnippet}...</p>
+      </div>
+      <img src="${picUrl}"alt="">
+      `;
+
+      document.getElementById("searchItemsList").append(div);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
