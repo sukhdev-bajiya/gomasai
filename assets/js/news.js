a7a6bf0294ecdd2f39c3f6a1979d39ba7a9aed5f
sukhdev-bajiya <bajiya4442@gmail.com>
2022-08-17 11:17:40 +0530

    done

--- assets/js/news.js
+++ assets/js/news.js
@@ -18,62 +18,84 @@ document.querySelector("form").addEventListener("submit", (event) => {
 showAllTime();
 
 function showAllTime() {
-  let val = document.getElementById("queryForFilter").value;
-  let from = document.getElementById("fromDateForFilter").value;
-  let to = document.getElementById("toDateForFilter").value;
-  let url = `/everything?q=${val}&from=${from}&to=${to}&sortBy=popularity&apiKey=aad0e1b4deab4ca4b409f7e74658e3cf`;
-  displayData(url);
+  let val = `https://gnews.io/api/v4/top-headlines?&token=169817000b938e4a84d28a8d84a20e79&country=in`;
+  displayData(val);
 }
 
 function showAllFiliterData() {
-  let val = document.getElementById("countryForFilter").value;
-  let from = document.getElementById("categoryForFilter").value;
-  let to = document.getElementById("languageForFilter").value;
-  let url = `/top-headlines?country=${val}&category=${from}&language=${to}&apiKey=aad0e1b4deab4ca4b409f7e74658e3cf`;
-  displayData(url);
+  let val = document.getElementById("queryForFilter").value;
+  let ext = document.getElementById("ShowErrorMessage");
+  if (val == "") {
+    ext.innerHTML = "The query is required";
+    ext.style.color = "red";
+  } else {
+    ext.innerHTML = "Query";
+    ext.style.color = "black";
+    let from = document.getElementById("fromDateForFilter").value;
+    let to = document.getElementById("toDateForFilter").value;
+    let cou = document.getElementById("countryForFilter").value;
+    let cat = document.getElementById("categoryForFilter").value;
+    let lan = document.getElementById("languageForFilter").value;
+    let url =
+      "https://gnews.io/api/v4/search?token=169817000b938e4a84d28a8d84a20e79";
+    if (val != "") {
+      url += `&q=${val}`;
+    }
+    if (from != "") {
+      url += `&from=${from}`;
+    }
+    if (to != "") {
+      url += `&to=${to}`;
+    }
+    if (cou != "") {
+      url += `&country=${cou}`;
+    }
+    if (cat != "") {
+      url += `&to=${cat}`;
+    }
+    if (lan != "") {
+      url += `&lang=${lan}`;
+    }
+
+    displayData(url);
+  }
 }
 
 async function displayData(val) {
   try {
-    let key = "aad0e1b4deab4ca4b409f7e74658e3cf";
-    let res = await fetch(`https://newsapi.org/v2${val}`);
+    let res = await fetch(`${val}`);
     let data = await res.json();
+    console.log(data);
     document.getElementById("showSearchNewsData").innerHTML = "";
     data.articles.forEach((element) => {
-      let atho;
-      if (element.author != null) {
-        atho = element.author.slice(0, 30) + "...";
-      } else {
-        atho = "gomasai";
-      }
-
       let div = document.createElement("div");
       div.addEventListener("click", () => {
         document.getElementById("showNewsOneINOneSection").style.display =
           "block";
         document.getElementById("showNewsOneINOneDiv").innerHTML = `
-        <img src="${
-          element.urlToImage || "./assets/img/masai.jpg"
-        }" alt="urlToImage">
-        <div>
-            <h1>${element.title}</h1>
-            <p>${element.description}</p>
+          <img src="${
+            element.image || "./assets/img/masai.jpg"
+          }" alt="urlToImage">
+          <div>
+              <h1>${element.title}</h1>
+              <p>${element.content}</p>
+              <h3>${element.publishedAt}</h3>
+              <a href="${element.url}">Go to news page</a>
+          </div>
 
-            <h2>${atho}</h2>
-            <h3>${element.publishedAt}</h3>
-        </div>
-        `;
+          
+          `;
       });
       div.innerHTML = `
-      <img src="${
-        element.urlToImage || "./assets/img/masai.jpg"
-      }" alt="urlToImage">
-      <div>
-          <h2>${element.title.slice(0, 70)}...</h2>
-          <p>${element.publishedAt}</p>
-      </div>
-      <span>${atho}</span>
-      `;
+        <img src="${
+          element.image || "./assets/img/masai.jpg"
+        }" alt="urlToImage">
+        <div>
+            <h2>${element.title.slice(0, 70)}...</h2>
+            <p>${element.publishedAt}</p>
+        </div>
+        <span>gnews</span>
+        `;
 
       document.getElementById("showSearchNewsData").append(div);
     });
