5955f7b5f1d3eea8be8298953b80799197ff5281
Sukhdev Bajiya <bajiya4442@gmail.com>
2022-10-28 21:31:54 +0530

    Translate page 70% Done

--- src/Redux/action.js
+++ src/Redux/action.js
@@ -1,6 +1,11 @@
 export const SET_USER = "SET_USER";
+export const SET_TRANSLATE = "SET_TRANSLATE";
 
 export const setUserlog = (payload) => ({
     type: SET_USER,
+    payload
+});
+export const setTranslate = (payload) => ({
+    type: SET_TRANSLATE,
     payload
 });
--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,9 +1,11 @@
 import {
-    SET_USER
+    SET_USER,
+    SET_TRANSLATE
 } from "./action";
 
 const initState = {
-    userlog: null
+    userlog: null,
+    translate: []
 };
 
 export const reducer = (state = initState, { type, payload }) => {
@@ -12,6 +14,12 @@ export const reducer = (state = initState, { type, payload }) => {
             return {
                 ...state,
                 userlog: payload
+            };
+
+        case SET_TRANSLATE:
+            return {
+                ...state,
+                translate: payload
             };
 
         default:
